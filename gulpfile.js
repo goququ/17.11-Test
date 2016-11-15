4b36dd551d89a44186e0413d94fc920ba679d346
qu <pukpukerok@gmail.com>
2016-11-15 18:37:26 +0000

    Добавил Bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
 'use strict';
 
 var gulp         = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    less         = require('gulp-less'),
-    path         = require('path'),
-    watch        = require('gulp-watch'),
-    browserSync  = require('browser-sync'),
-    reload       = browserSync.reload,
-    rigger       = require('gulp-rigger');
+	autoprefixer = require('gulp-autoprefixer'),
+	less         = require('gulp-less'),
+	path         = require('path'),
+	watch        = require('gulp-watch'),
+	browserSync  = require('browser-sync'),
+	reload       = browserSync.reload,
+	rigger       = require('gulp-rigger');
 
 
 gulp.task('html', function () {
 	return gulp.src('src/html/*.html')
-	    .pipe(rigger())
+		.pipe(rigger())
 		.pipe(gulp.dest('dist'))
 		.pipe(reload({stream: true}));
 });
 
 gulp.task('styles', function () {
 	return gulp.src(['src/styles/style.less', 'node_modules/normalize.css/normalize.css'])
-	    .pipe(rigger())
-	    .pipe(less({
-          paths: [ path.join(__dirname, 'less', 'includes') ]
-        }))
+		.pipe(rigger())
+		.pipe(less({
+		  paths: [ path.join(__dirname, 'less', 'includes') ]
+		}))
 		.pipe(gulp.dest('dist/css'))
 		.pipe(reload({stream: true}));
 });
@@ -41,34 +41,45 @@ gulp.task('img', function () {
 		.pipe(reload({stream: true}));
 });
 
+gulp.task('bootstrap', function () {
+	return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.min.css', 
+					'node_modules/bootstrap/dist/js/bootstrap.min.js'])
+		.pipe(gulp.dest('dist/bootstrap'));
+});
+
+gulp.task('fonts', function () {
+	return gulp.src(['src/fonts/*', 'node_modules/bootstrap/fonts/*'])
+		.pipe(gulp.dest('dist/bootstrap/fonts'));
+});
+
 
 gulp.task('watch', function(){
 
-    watch( ['src/styles/*/*'], function(event, cb) {
-        gulp.start('styles');
-    }).on('change', browserSync.reload);
-    
-    watch(['src/html/*/*.html', 'src/html/*.html'], function(event, cb) {
-        gulp.start('html');
-    });
-    watch(['src/js/*.js'], function(event, cb) {
-        gulp.start('js');
-    });
+	watch( ['src/styles/*/*'], function(event, cb) {
+		gulp.start('styles');
+	}).on('change', browserSync.reload);
+	
+	watch(['src/html/*/*.html', 'src/html/*.html'], function(event, cb) {
+		gulp.start('html');
+	});
+	watch(['src/js/*.js'], function(event, cb) {
+		gulp.start('js');
+	});
 });
 
 //Server
 var config = {
-    server: {
-        baseDir: "dist"
-    },
-    tunnel: true,
-    host: 'localhost',
-    port: 8080,
-    logPrefix: 'qu'
+	server: {
+		baseDir: "dist"
+	},
+	tunnel: true,
+	host: 'localhost',
+	port: 8080,
+	logPrefix: 'qu'
 };
 
 gulp.task('server', function () {
-    browserSync(config);
+	browserSync(config);
 });
 
-gulp.task('default', ['html', 'styles', 'img', 'js', 'server', 'watch']);
+gulp.task('default', ['html', 'styles', 'img', 'js', 'bootstrap','fonts','server', 'watch']);
